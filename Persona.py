a14234127e61e0950827693471ac9496a4b1e5da
Diego Martinez <vuzulusacion@gmail.com>
2025-11-20 10:09:00 -0500

    feat: se crearon los archivos Persona y Listas ademas que a persona se le delego una clase

---
+++ Persona.py
@@ -0,0 +1,8 @@
+class Persona:
+    def __init__(self,nombre,apellido,ciudad,edad,correo):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.ciudad=ciudad
+        self.edad=edad
+        self.correo=correo
+
