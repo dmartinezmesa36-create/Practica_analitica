375be89b04eadae759b2552ef27328bfce0fac1b
Diego Martinez <Diego.martinez.mesa36@gmail.com>
2025-11-21 14:08:36 -0500

    fead: se agregaron las opciones al menu y se  genero una soloa unica lista para simplificar la insercion de datos

--- Persona.py
+++ Persona.py
@@ -1,43 +1,56 @@
+from typing import List
+
 class Persona:
-
-    
-    def __init__(self,nombre,apellido,ciudad,edad,correo):
-        self.nombre=nombre
-        self.apellido=apellido
-        self.ciudad=ciudad
-        self.edad=edad
-        self.correo=correo
-        pass
-
-    lista_nombres = []
-    lista_apellidos = []
-    lista_cidad = []
-    lista_edad = []
-    lista_correo = []
-
-    def agregar_nombre(self):
-        Persona.lista_nombres.append(self.nombre)
-        print(f"{self.nombre} agregado correctamente.")
-
-    def agregar_apellido(self):
-        Persona.lista_apellidos.append(self.apellido)
-        print(f"{self.apellido} agregado correctamente.")
-    
-    def agregar_ciudad(self):
-        Persona.lista_cidad.append(self.ciudad)
-        print(f"{self.ciudad} agregado correctamente.")
-
-    def agregar_edad(self):
-        Persona.lista_edad.append(self.edad)
-        print(f"{self.edad} agregado correctamente.")
-    
-    def agregar_edad(self):
-        Persona.lista_correo.append(self.correo)
-        print(f"{self.correo} agregado correctamente.")
-    
+    def __init__(self, nombre, apellido, ciudad, edad, correo, documento):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.ciudad = ciudad
+        self.edad = edad
+        self.correo = correo
+        self.documento = documento
 
 
-    
+# --- Funciones del menú ---
+
+
+def agregar_persona(personas: List[Persona]):
+    nombre = input("Nombre: ")
+    apellido = input("Apellido: ")
+    ciudad = input("Ciudad: ")
+    edad = int(input("Edad: "))
+    while True:
+        if edad < 0 and edad >= 120:
+            print("La edad no puede ser negativa ")
+            return
+        else:
+            print("Edad válida ")
+            break
+    correo = input("Correo: ")
+    while True:
+        if "@" in correo and "." in correo:
+            print("Correo válido ")
+            break
+        else:
+            print("Correo inválido , ingrese nuevamente")
+            correo = input("Correo: ")
+    documento = int(input("Documento: "))
+    if documento % 2 == 0: 
+        print("El documento debe ser impar ")
+        return
+
+
+    nueva = Persona(nombre, apellido, ciudad, edad, correo, documento)
+    personas.append(nueva)
+    print("Persona agregada correctamente ")
+
+def listar_personas(personas: List[Persona]):
+    if not personas:
+        print("No hay personas registradas ")
+    else:
+        for p in personas:
+            print(p.nombre, p.apellido, p.edad, p.ciudad, p.correo)
+
+
 
 
 
